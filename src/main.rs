81731612016d16d22aab12bd7e7316234fb9dcec
Leonardo Alonso <leonardoalonsososa@gmail.com>
2022-07-24 17:27:59 -0500

    select last

--- src/main.rs
+++ src/main.rs
@@ -23,32 +23,32 @@ fn main() {
     use self::schema::posts;
     use self::schema::posts::dsl::*;
 
-    let new_post = NewPost {
-        title: "Mi segundo post",
-        body: "lorem ipsum",
-        slug: "segundo-post"
-    };
-    let post: Post = diesel::insert_into(posts::table).values(&new_post).get_result(&con).expect("Error insierting data");
+    // let new_post = NewPost {
+    //     title: "Mi segundo post",
+    //     body: "lorem ipsum",
+    //     slug: "segundo-post"
+    // };
+    // let post: Post = diesel::insert_into(posts::table).values(&new_post).get_result(&con).expect("Error insierting data");
 
     //Select * from post
     println!("Query sin limites");
     let post_result = posts.load::<Post>(&con).expect("Error on query excecution");
     
     for post in post_result {
-        println!("{}", post.title);
+        println!("{:?}", post);
     }
 
-    // println!("Query con limites");
-    // let post_result = posts.limit(1).load::<Post>(&con).expect("Error on query excecution");
+    println!("Query con limites");
+    let post_result = posts.order(id.desc()).limit(1).load::<Post>(&con).expect("Error on query excecution");
 
-    // for post in post_result {
-    //     println!("{}", post.title);
-    // }
-
-    // Query con columnas especificas
-    let post_result = posts.select((title, body)).load::<PostSimplicado>(&con).expect("Error on query excecution");
     for post in post_result {
         println!("{:?}", post);
     }
+
+    // // Query con columnas especificas
+    // let post_result = posts.select((title, body)).load::<PostSimplicado>(&con).expect("Error on query excecution");
+    // for post in post_result {
+    //     println!("{:?}", post);
+    // }
 
 }
